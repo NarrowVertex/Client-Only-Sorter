4a869aa3a73514a977c2854980b75d819bf8c341
NarrowVertex <walterchoi3916@gmail.com>
2024-08-02 03:45:29 +0900

    Change swap method

---
+++ Common/src/main/java/com/narrowvertex/cos/mixin/ContainerScreenInterfaceMixin.java
@@ -0,0 +1,16 @@
+package com.narrowvertex.cos.mixin;
+
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.world.inventory.Slot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(AbstractContainerScreen.class)
+public interface ContainerScreenInterfaceMixin {
+
+    @Accessor("hoveredSlot")
+    Slot getSlot();
+
+    @Accessor("hoveredSlot")
+    void setSlot(Slot slot);
+}
